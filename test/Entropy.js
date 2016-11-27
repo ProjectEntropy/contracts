c271a827f21f3053d18c1dbcc86cba59d71e9f1d
Joran <joran.k@gmail.com>
2016-11-27 15:37:38 +1300

    negative test cases around guardian/citizenship on creation

--- test/Entropy.js
+++ test/Entropy.js
@@ -1,11 +1,26 @@
 contract('Entropy', (accounts) => {
 
+  var creator_guardian  = web3.eth.accounts[0];
+  var stranger          = web3.eth.accounts[1];
+
+  // Creation / Setup
   it("makes creator a Citizen", function(done) {
     deployEntropyContract()
       .then((entropy) => {
-        entropy.balanceOf(web3.eth.accounts[0]).then((balance) => {
+        entropy.balanceOf(creator_guardian).then((balance) => {
           assert.equal(balance, '1', false, "Did \
           not give citizenship token");
+          done();
+        })
+      })
+  });
+
+  it("keeps strangers as not yet Citizens", function(done) {
+    deployEntropyContract()
+      .then((entropy) => {
+        entropy.balanceOf(stranger).then((balance) => {
+          assert.equal(balance, '0', false, "Shouldn not have given \
+           citizenship token!");
           done();
         })
       })
@@ -14,9 +29,20 @@ contract('Entropy', (accounts) => {
   it("makes creator a Guardian", function(done) {
     deployEntropyContract()
       .then((entropy) => {
-        entropy.isGuardian(web3.eth.accounts[0]).then((guardian) => {
+        entropy.isGuardian(creator_guardian).then((guardian) => {
           assert.equal(guardian, true, false, "Did \
           not set as guardian");
+          done();
+        })
+      })
+  });
+
+  it("keeps strangers as not Guardian", function(done) {
+    deployEntropyContract()
+      .then((entropy) => {
+        entropy.isGuardian(stranger).then((guardian) => {
+          assert.equal(guardian, false, false, "Should \
+          not have set stranger as guardian!");
           done();
         })
       })
